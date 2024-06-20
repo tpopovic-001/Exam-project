d58a46af7e8f9bd6c7b4f9a3b5f01e37478f9ea6
tpopovic-001 <teodorpopovic001@gmail.com>
2024-06-20 15:49:11 +0200

    Models created with database first method, server pagination, sorting and search functionality has been added for one page (Index.cshtml).

--- PC_Components_Shop/Controllers/HomeController.cs
+++ PC_Components_Shop/Controllers/HomeController.cs
@@ -1,30 +1,43 @@
-﻿using System;
+﻿using PC_Components_Shop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
+using System.Data.SqlClient;
 
 namespace PC_Components_Shop.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string filterCriteria, int? page, string sortByPrice)
         {
-            return View();
-        }
+            ExamProjectDBEntities ExPe = new ExamProjectDBEntities();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            List<Product> Products = ExPe.Products.Where(w => w.ProductName.StartsWith(filterCriteria) || filterCriteria == null).ToList();
+            IPagedList<Product> PagedProducts = Products.ToPagedList(page ?? 1,10);
 
-            return View();
-        }
+            switch (sortByPrice)
+            {
+                case "standardPrice_asc":
+                    PagedProducts = Products.OrderBy(o => o.StandardPrice).ToPagedList(page ?? 1, 10);
+                    break;
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                case "standardPrice_desc":
+                    PagedProducts = Products.OrderByDescending(o => o.StandardPrice).ToPagedList(page ?? 1, 10);
+                    break;
 
-            return View();
+                case "default":
+                    sortByPrice = null;
+                    break;
+            }
+            ViewBag.CurrentSort = sortByPrice;
+            ViewBag.PriceSortParm = sortByPrice == "standardPrice_asc" ? "standardPrice_asc" : "standardPrice_desc";
+         
+            return View(PagedProducts);  
         }
     }
 }
